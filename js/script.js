d9d27aa29345abed4a87f1fec9e5b46af1505e42
Rosa-M-FS <rmlopezrobles@gmail.com>
2024-11-06 00:06:15 +0100

    Ejercicio Terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
  // Aquí tu código
+const lista=document.getElementById("lista");
+const agregar=document.getElementById("agregar")
+agregar.addEventListener("click", function(){
+    let addLista=prompt("Escribe el elemento que quieres añadir a la lista");
+    let newElemento=document.createElement("li")
+    newElemento.textContent=addLista;
+    lista.appendChild(newElemento);
+});
