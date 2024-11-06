efb7607532f51a809abf2d917a531cc90d0b1645
Rosa-M-FS <rmlopezrobles@gmail.com>
2024-11-06 10:10:08 +0100

    Ejercicio Terminado Mejorado

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const lista=document.getElementById("lista");
 const agregar=document.getElementById("agregar")
 agregar.addEventListener("click", function(){
     let addLista=prompt("Escribe el elemento que quieres añadir a la lista");
-    let newElemento=document.createElement("li")
-    newElemento.textContent=addLista;
-    lista.appendChild(newElemento);
+    if (addLista){
+        let newElemento=document.createElement("li")
+        newElemento.textContent=addLista;
+        lista.appendChild(newElemento); 
+    }else{
+        alert("No has introducido ningun elemento a la lista")
+    }
 });
